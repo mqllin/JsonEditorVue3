ca488f39660b41af763c9043fa54cb666ebca37a
Lin <584363361@qq.com>
2024-10-09 15:33:51 +0800

    update README.md

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/',
-      name: 'about',
-      component: () => import('../views/EditorView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutView
+        },
+        {
+            path: '/',
+            name: 'about',
+            component: () => import('../views/EditorView.vue')
+        },
+        {
+            path: '/EditorView',
+            name: 'jsonEditor',
+            component: () => import('../views/EditorView.vue')
+        }
+
+    ]
 })
 
 export default router
